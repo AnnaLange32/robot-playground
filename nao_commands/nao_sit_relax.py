cbe810b6090d67efcd698491fecb97cfe25b5f65
AnnaLange32 <anna_lange@hotmail.de>
2021-11-16 16:15:08 +0100

    adding basic nao code

---
+++ nao_commands/nao_sit_relax.py
@@ -0,0 +1,23 @@
+
+import sys
+sys.path.append("/home/anna/pynaoqi-python2.7-2.5.7.1-linux64/lib/python2.7/site-packages")
+from naoqi import ALProxy
+
+def main():
+
+    PORT=9559
+    robotIP = "172.20.10.5" #"192.168.43.7"
+    motionProxy  = ALProxy("ALMotion", robotIP, PORT)
+    posture_service = ALProxy("ALRobotPosture", robotIP, PORT)
+
+
+    # Go to rest position
+    #motionProxy.StandZero(
+    posture_service.goToPosture("SitRelax", 0.5)	
+
+    print "::::::::::::::::::::::::::::::::::::::"
+    print "          NAO movement done          "
+    print "::::::::::::::::::::::::::::::::::::::"
+
+if __name__ == "__main__":
+    main()
---
+++ nao_commands/nao_speech.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append("/home/anna/pynaoqi-python2.7-2.5.7.1-linux64/lib/python2.7/site-packages")
+
+from naoqi import ALProxy
+import time
+
+def main():
+
+
+	IP, PORT = "172.20.10.5", 9559 
+	tts = ALProxy("ALTextToSpeech", IP, PORT)
+	animation = ALProxy("ALAnimationPlayer", IP, PORT)
+	posture = ALProxy("ALRobotPosture", IP, PORT)
+	tts.setLanguage("English")
+
+	user_msg = "Hello. I am Nao. We will start the experiment soon."
+	#user_msg2 = "Before starting, I would like to ask a question."
+	#animation.run("animations/Stand/Gestures/Give_3", _async=True)
+
+	user_msg3 = "Hello Hanna, how was your work today?"
+	
+	#tts.say(str(user_msg)
+	#tts.say(str(user_msg2))
+	time.sleep(3)
+	tts.say(str(user_msg3))
+
+	
+
+
+if __name__ == '__main__':
+	main()
